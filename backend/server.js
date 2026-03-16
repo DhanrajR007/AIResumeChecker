a0369ce7b83deaa6e418df048b8ce0418946c410
DhanrajR007 <dhanrajrathore1111@gmail.com>
2026-03-16 23:28:17 +0530

    add interview report generation feature with Zod validation and integrate Google GenAI API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const app = require("./src/app.js");
 const conectDB = require("./src/config/db.js");
+const main = require("./src/services/ai.service.js");
 
 conectDB();
+main.generateInterviewReport({
+  resume: "Experienced software engineer with a strong background in full-stack development, specializing in JavaScript, React, and Node.js. Proven track record of delivering high-quality web applications and collaborating effectively in agile teams.",
+  selfDescription: "I am a passionate software engineer with over 5 years of experience in building scalable web applications. I enjoy solving complex problems and continuously learning new technologies to improve my skills.",
+  jobDescription: "We are looking for a skilled software engineer with expertise in JavaScript, React, and Node.js to join our dynamic team. The ideal candidate should have experience in developing high-quality web applications and a strong understanding of agile methodologies.",
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
---
+++ backend/src/model/interviewReport.model.js
@@ -0,0 +1,101 @@
+const mongoose = require("mongoose");
+const technicalQuestionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: [true, "Question is required"],
+    },
+    intention: {
+      type: String,
+      required: [true, "Intention is required"],
+    },
+    answer: {
+      type: String,
+      required: [true, "Answer is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const behavioralQuestionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: [true, "Question is required"],
+    },
+    intention: {
+      type: String,
+      required: [true, "Intention is required"],
+    },
+    answer: {
+      type: String,
+      required: [true, "Answer is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const skillGapSchema = new mongoose.Schema(
+  {
+    skill: {
+      type: String,
+      required: [true, "Skill is required"],
+    },
+    severity: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      required: [true, "Severity is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
+
+const preprationPlanSchema = new mongoose.Schema({
+  day: {
+    type: Number,
+    required: [true, "Day is required"],
+  },
+  focus: {
+    type: String,
+    required: [true, "Focus is required"],
+  },
+  tasks: [
+    {
+      type: String,
+      required: [true, "Task is required"],
+    },
+  ],
+});
+
+const interviewReportSchema = new mongoose.Schema({
+  jobDescription: {
+    type: String,
+    required: [true, "Job description is required"],
+  },
+  resume: {
+    type: String,
+  },
+  selfDiscription: {
+    type: String,
+  },
+  matchScore: {
+    type: Number,
+    min: 0,
+    max: 100,
+  },
+  technicalQuestions: [technicalQuestionSchema],
+  behavioralQuestions: [behavioralQuestionSchema],
+  skillGap: [skillGapSchema],
+  preprationPlan: [preprationPlanSchema],
+});
+
+
+const InterviewReport = mongoose.model("InterviewReport", interviewReportSchema);
+
+module.exports = InterviewReport;
